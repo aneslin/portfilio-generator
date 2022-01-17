23e9242d2469e1067df964d2dcc4b40ef9525130
Aaron Neslin <aneslin125@gmail.com>
2022-01-17 16:52:32 -0500

    compartmentalized functions

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-const fs = require('fs')
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+
 const [nameG, github] = profileDataArgs;
+const generatePage = require('./src/page-template');
 
 
 
-
-const generatePage = (userName, githubName) => {return`
-<!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${userName}</h1>
-    <h2><a href="https://github.com/${githubName}">Github</a></h2>
-  </body>
-  </html>`};
 
 
   fs.writeFile('index.html', generatePage(nameG, github), err => {

f0c781b697cc016dd5c409bd65a5dfd9e21f23da
Aaron Neslin <aneslin125@gmail.com>
2022-01-19 20:31:26 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [nameG, github] = profileDataArgs;
+const inquirer = require("inquirer");
 const generatePage = require('./src/page-template');
 
 
+inquirer.prompt([
+  {type:'input',
+  name:"name",
+  message: "What is your name?"}
+]).then(answers=>console.log(answers));
 
 
 
-  fs.writeFile('index.html', generatePage(nameG, github), err => {
-      if (err) throw err;
-      console.log('Portfolio complete! Check out index.html to see the output!');
+
+  // fs.writeFile('index.html', generatePage(nameG, github), err => {
+  //     if (err) throw err;
+  //     console.log('Portfolio complete! Check out index.html to see the output!');
       
-  });
+  // });
